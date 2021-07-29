f49a7dcc050a1810063dd16dec8c57b9566ab7e7
Sui572 <87964958+Sui572@users.noreply.github.com>
2021-07-29 15:14:21 +0900

    Initial commit

---
+++ src/practice/Practice2.tsx
@@ -0,0 +1,17 @@
+export const Practice2 = () => {
+  const getTotalfee = (num: number): number => {
+    const total = num * 1.1;
+    return total;
+  };
+
+  const onClickPractice = () => {
+    console.log(getTotalfee(1000));
+  };
+
+  return (
+    <div>
+      <p>練習問題：返却地の型指定</p>
+      <button onClick={onClickPractice}>練習問題2を実行</button>
+    </div>
+  );
+};
